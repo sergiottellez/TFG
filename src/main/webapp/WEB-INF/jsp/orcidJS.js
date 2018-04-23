c73030c21ee3991918d5763230b489c4091b78ed
Sergio Torres Téllez <sergiottellez@192.168.1.108>
2018-04-23 17:20:10 +0200

    ORCID JS

--- src/main/webapp/WEB-INF/jsp/orcidJS.js
+++ src/main/webapp/WEB-INF/jsp/orcidJS.js
@@ -7,7 +7,7 @@
 var oauthWindow;
 
 function openORCID() {
-    var oauthWindow = window.open("https://sandbox.orcid.org/oauth/authorize?client_id=0000-0002-1223-3173&response_type=code&scope=/authenticate&show_login=false&redirect_uri=http://localhost:8080/oauth-redirect.html", "_blank", "toolbar=no, scrollbars=yes, width=500, height=600, top=500, left=500");
+    var oauthWindow = window.open("https://orcid.org/oauth/authorize?client_id=APP-L3BFXY1PC98OL94Y&response_type=code&scope=/authenticate&redirect_uri=http://localhost:8080/TFGPruebaFinal/", "_blank", "toolbar=no, scrollbars=yes, width=500, height=600, top=500, left=500");
 }
 
 

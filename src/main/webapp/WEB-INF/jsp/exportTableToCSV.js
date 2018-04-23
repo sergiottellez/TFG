20af8bd4716b7f68a787d38c1f8605cb34b29279
Sergio Torres Téllez <sergiottellez@192.168.1.108>
2018-04-23 19:41:43 +0200

    Funcions JS para exportar a CSV

--- src/main/webapp/WEB-INF/jsp/exportTableToCSV.js
+++ src/main/webapp/WEB-INF/jsp/exportTableToCSV.js
@@ -48,4 +48,30 @@ function exportTableToCSV(filename) {
     downloadCSV(csv.join("n"), filename);
 }
 
+function ExportToExcel(htmlExport) {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    
+    //other browser not tested on IE 11
+    // If Internet Explorer
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))
+    {
+        jQuery('body').append(" <iframe id=\"iframeExport\" style=\"display:none\"></iframe>");
+        iframeExport.document.open("txt/html", "replace");
+        iframeExport.document.write(htmlExport);
+        iframeExport.document.close();
+        iframeExport.focus();
+        sa = iframeExport.document.execCommand("SaveAs", true, "List.xls");
+    }
+    else {      
+        var link = document.createElement('a');
+    
+        document.body.appendChild(link); // Firefox requires the link to be in the body
+        link.download = "List.xls";
+        link.href = 'data:application/vnd.ms-excel,' + escape(htmlExport);
+        link.click();
+        document.body.removeChild(link);
+    }
+}
+
 

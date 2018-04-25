08a15a27cc174a8637d0e60e8c831afd08e733ac
Sergio Torres Téllez <sergiottellez@192.168.1.108>
2018-04-25 18:44:47 +0200

    JavaScript

--- src/main/webapp/WEB-INF/jsp/generarTexto.js
+++ src/main/webapp/WEB-INF/jsp/generarTexto.js
@@ -46,12 +46,12 @@ function generarTexto(emails) {
  
   //texto.push(datos.nombre);
   //texto.push(';\n');
-  var email = [emails];
+  var email = emails;
   var i;
   for(i=0;i<email.length;i++){
       
       var introdu = email[i].toString();
-      var muestra = introdu.substring(7);
+      var muestra = introdu.substring(8);
       texto.push(muestra);
       
   }

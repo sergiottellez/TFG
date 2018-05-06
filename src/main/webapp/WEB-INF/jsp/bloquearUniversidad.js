4161c365d02150e17891336d6455db9b3afe901a
Sergio Torres Téllez <sergiottellez@192.168.1.108>
2018-05-06 18:46:18 +0200

    Cambio formato emails

---
+++ src/main/webapp/WEB-INF/jsp/bloquearUniversidad.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    function bloqueaUniversidad(){
+    if($("#empresa").val() == '1'){
+        $("#universidad").prop("disabled",true);
+    }else{
+                $("#universidad").prop("disabled",false);
+
+    }
+    
+}
+     

bdcde6b0d7003c6eb015c8729aeca89d5ccd4d17
Sergio Torres Téllez <sergiottellez@192.168.1.108>
2018-05-06 13:53:36 +0200

    Confirmacion de mensajes

--- src/main/webapp/WEB-INF/jsp/confirmar.js
+++ src/main/webapp/WEB-INF/jsp/confirmar.js
@@ -16,3 +16,7 @@ function confirmar()
 function ayuda(){
            alert('Si usted tiene alguna duda o sugerencia, contacte con: sistedestfg@gmail.com.');
        }
+       
+function ayudaNewUsuario(){
+           alert('Si el usuario es externo, su ID es "EXT". En otro caso seguirá este patrón SxxxxxN (o F en caso de fundador)');
+       }
